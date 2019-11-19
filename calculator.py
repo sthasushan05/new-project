b67183798c5e695e1c89ef88c695308c0d94bdb4
sthasushan05 <sthasushan05@gmail.com>
2019-11-19 07:39:18 +0545

    class 6

--- calculator.py
+++ calculator.py
@@ -18,44 +18,101 @@ while int(age)<15:
     age=input("enter your age\n")
 mob_no=input("enter your number\n")
 while len(mob_no)!=10:
-    print("the correct number")
-    mob_no=input("enter your number\n")
-
+    if mob_no.isdecimal()==True:
+        print("the correct number")
+        mob_no=input("enter your number\n")
+    else:
+        print("invalid")
+        mob_no=input("enter the number")
 #calculator
 
+def calculator():
+    sum=number_1+number_2
+    print('{} + {} = '.format(number_1, number_2), number_1 + number_2)
+    return sum
 
-print("choose the number of the operataon you want\n1.addition\n2.subtraction\n3.division\n4.multiplication\n5.square\n6.cube\n7.square root\n8.calander")
+print("choose the number of the operataon you want\n1.addition\n2.subtraction\n3.division\n4.multiplication\n5.square\n6.cube\n7.square root\n8.cube root\n9.calander")
 select=int(input("select the number: \n"))
-
-if(int(select==1)):
-    number_1 = int(input('Enter your first number: '))
-    number_2 = int(input('Enter your second number: '))
-    print('{} + {} = '.format(number_1, number_2),number_1 + number_2)
-elif(int(select==2)):
-    number_1 = int(input('Enter your first number: '))
-    number_2 = int(input('Enter your second number: '))
-    print('{} - {} = '.format(number_1, number_2),number_1 - number_2)
-elif(int(select == 3)):
-    number_1 = int(input('Enter your first number: '))
-    number_2 = int(input('Enter your second number: '))
-    print('{} / {} = '.format(number_1, number_2), number_1 / number_2)
-elif(int(select==4)):
-    number_1 = int(input('Enter your first number: '))
-    number_2 = int(input('Enter your second number: '))
-    print('{} * {} = '.format(number_1, number_2),number_1 * number_2)
-elif(int(select==5)):
-    number_1 = int(input('Enter your number: '))
-    print('{}² = '.format(number_1),number_1 * number_1)
-elif (int(select == 6)):
-    number_1 = int(input('Enter your number: '))
-    print('{}³ = '.format(number_1), number_1 * number_1 *number_1)
-elif (int(select == 7)):
-    number_1 = int(input('Enter your first number: '))
-    print('{}½ = '.format(number_1), number_1 /0.5)
-elif(int(select==8)):
-    yy=int(input("enter the year"))
-    mm=int(input("enter the month"))
-    print(calendar.month(yy , mm))
-else:
-    print("the number you dial is incorrect please try again")
-    sys.exit()
+select = 0
+while select ==0:
+    if(int(select==1)):
+        number_1 = int(input('Enter your first number: '))
+        number_2 = int(input('Enter your second number: '))
+        print('{} + {} = '.format(number_1, number_2),number_1 + number_2)
+        choice=int(input("1.menu\n2.exit"))
+        if choice==1:
+            calculator()
+        else:
+            sys.exit("thanks for using")
+    elif(int(select==2)):
+        number_1 = int(input('Enter your first number: '))
+        number_2 = int(input('Enter your second number: '))
+        print('{} - {} = '.format(number_1, number_2),number_1 - number_2)
+        choice = input("1.menu\n2.exit")
+        if choice == 1:
+            calculator()
+        else:
+            sys.exit("thanks for using")
+    elif(int(select == 3)):
+        number_1 = int(input('Enter your first number: '))
+        number_2 = int(input('Enter your second number: '))
+        print('{} / {} = '.format(number_1, number_2), number_1 / number_2)
+        choice = input("1.menu\n2.exit")
+        if choice == 1:
+            calculator()
+        else:
+            sys.exit("thanks for using")
+    elif(int(select==4)):
+        number_1 = int(input('Enter your first number: '))
+        number_2 = int(input('Enter your second number: '))
+        print('{} * {} = '.format(number_1, number_2),number_1 * number_2)
+        choice = input("1.menu\n2.exit")
+        if choice == 1:
+            calculator()
+        else:
+            sys.exit("thanks for using")
+    elif(int(select==5)):
+        number_1 = int(input('Enter your number: '))
+        print('{}² = '.format(number_1),number_1 * number_1)
+        choice = input("1.menu\n2.exit")
+        if choice == 1:
+            calculator()
+        else:
+            sys.exit("thanks for using")
+    elif (int(select == 6)):
+        number_1 = int(input('Enter your number: '))
+        print('{}³ = '.format(number_1), number_1 * number_1 *number_1)
+        choice = input("1.menu\n2.exit")
+        if choice == 1:
+            calculator()
+        else:
+            sys.exit("thanks for using")
+    elif (int(select == 7)):
+        number_1 = int(input('Enter your first number: '))
+        print('{}½ = '.format(number_1), number_1 **0.5)
+        choice = input("1.menu\n2.exit")
+        if choice == 1:
+            calculator()
+        else:
+            sys.exit("thanks for using")
+    elif(int(select==8)):
+        number_1=int(input("enter the number you want to cube"))
+        print('{}**()1/3 = '.format(number_1),number_1**(1/3))
+        choice = input("1.menu\n2.exit")
+        if choice == 1:
+            calculator()
+        else:
+            sys.exit("thanks for using")
+    elif(int(select==9)):
+        yy=int(input("enter the year"))
+        mm=int(input("enter the month"))
+        print(calendar.month(yy , mm))
+        choice = input("1.menu\n2.exit")
+        if choice == 1:
+            calculator()
+        else:
+            sys.exit("thanks for using")
+    else:
+        print("the number you dial is incorrect please try again")
+        sys.exit()
+calculator()
---
+++ test 7.py
@@ -0,0 +1,55 @@
+#area of circle
+'''def radius(r):
+    area=22/7*r*r
+    return area
+r=input("enter the radius of circle")
+area=radius(float(r))
+print("son of  a bitch",area)'''
+
+
+#pow of number
+'''def pow(a,b):
+    power=a**b
+    return power
+a=input("enter the vlaue")
+b=input("enter the power")
+power=pow(float(a),float(b))
+print("fuck off",power)'''
+
+
+#armstrong number
+'''def a_strong(a):
+    sum=0
+    while a>0:
+        num=a%10
+        sum=sum+num**3
+        a//=10
+    return sum
+
+
+
+strong=int(input("enter the armstrongg number"))
+goat=a_strong(strong)
+if goat==strong:
+    print("hello world")
+else:
+    print("fuck off")'''
+
+
+#palindrome
+def pal(p):
+    rev=0
+    while p>0:
+        num=p%10
+        rev=rev*10+num
+        p=p//10
+    return rev
+
+
+
+pal_num=int(input("enter the number"))
+ohaiyo=pal(pal_num)
+if ohaiyo==pal_num:
+    print("ohiyogosaimas")
+else:
+    print("baka")
